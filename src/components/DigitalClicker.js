3dbece9634d9b70fe67c42875010f80891824423
Monica Tamaru Jones <mntamaru@gmail.com>
2018-10-17 06:04:51 +0000

    automatically backed up by learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -10,18 +10,17 @@ export default class DigitalClicker extends React.Component {
   }
 
   handleClick = () => {
-<<<<<<< HEAD
+
     this.setState ({timesClicked: this.state.timesClicked += 1}, () => (this.state.timesClicked))
   }
   
   render() {
     return (<div><button onClick={this.handleClick}> {this.state.timesClicked}</button></div>)
-=======
+
     this.setState ({timesClicked: this.state.timesClicked + 1})
   }
   
   render() {
     return (<button onClick={this.handleClick}> {this.state.timesClicked}</button>)
->>>>>>> e884a9a219831d615faf451f5bbee8980bc2cd4b
   }
 }
